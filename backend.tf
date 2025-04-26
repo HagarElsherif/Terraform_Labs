f82217a17c91dd199f31fbef1bd7376690da7002
Hagar Elsherif <hagar.elsherif@gmail.com>
2025-04-26 21:17:34 +0300

    first commit

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "backend-hagar"      
+    key    = "statefile.tfstate"  
+    region = "us-east-1"   
+    use_lockfile = true                                     
+  }
+}
---
+++ security groups.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "bastion_sg" {
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_security_group" "app_sg" {
+  
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.my_vpc.cidr_block]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.my_vpc.cidr_block]
+  }
+
+ 
+}
+
